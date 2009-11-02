e6abaf560ef2b4aadb4c97dc2ef6ef90b697fa30
andreas.duchen <andreas.duchen@aefdbfa2-c794-11de-8410-e5b1e99fc78e>
2009-11-02 15:40:35 +0000

    First Audio test! Best audio ever!

---
+++ trunk/Include/AudioManager.h
@@ -0,0 +1,26 @@
+/**
+ *    ___  _                __  __                 _
+ *   / _ )(_)__ __ _  __ __/ /_/ /  ___ ___  ___ _(_)__  ___
+ *  / _  / (_-</  ' \/ // / __/ _ \/ -_) _ \/ _ `/ / _ \/ -_)
+ * /____/_/___/_/_/_/\_,_/\__/_//_/\__/_//_/\_, /_/_//_/\__/
+ *                                         /___/
+ *
+ * @file BismuthAudio.h
+ */
+
+#pragma once
+
+namespace BismuthAudio {
+
+	/**
+	 * BismuthAudio class
+	 */
+	class AudioManager {
+	public:
+		AudioManager();
+		virtual ~AudioManager();
+		int test();
+	private:
+	};
+
+}
