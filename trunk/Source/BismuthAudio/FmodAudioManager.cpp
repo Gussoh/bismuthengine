967e3c62dd82d193804b434dcc1d479d01b8c088
andreas.duchen <andreas.duchen@aefdbfa2-c794-11de-8410-e5b1e99fc78e>
2009-11-09 14:50:10 +0000

    Added FmodAudioManager files

---
+++ trunk/Source/BismuthAudio/FmodAudioManager.cpp
@@ -0,0 +1,82 @@
+/**
+ * @file Template.cpp
+ */
+
+#include "stdafx.h"
+#include "FmodAudioManager.h"
+#include "fmod_errors.h"
+using namespace Bismuth;
+using namespace Bismuth::Audio;
+
+/** 
+* Initialise the Fmod system
+*/
+FmodAudioManager::FmodAudioManager() {
+	FMOD_RESULT result;
+
+	result = FMOD::System_Create(&fmodSystem);		// Create the main system object.
+	if (result != FMOD_OK)
+	{
+		printf("FMOD error! (%d) %s\n", result, FMOD_ErrorString(result));
+		
+		throw std::exception(); // TODO: change to the correct exception
+	}
+
+	result = fmodSystem->init(100, FMOD_INIT_NORMAL, 0);	// Initialize FMOD.
+	if (result != FMOD_OK)
+	{
+		printf("FMOD error! (%d) %s\n", result, FMOD_ErrorString(result));
+		
+		throw std::exception();
+	}
+	
+
+}
+
+/**
+* Close the Fmod system.
+*/
+FmodAudioManager::~FmodAudioManager() {
+
+}
+
+void FmodAudioManager::playSound(Entity &entity, AudioProperties &properties) {
+
+}
+
+/*
+int AudioManager::test () {
+
+	FMOD_RESULT result;
+	FMOD::Sound *sound;
+
+	result = fmodSystem->createSound("../../Media/Audio/jaguar.wav", FMOD_LOOP_NORMAL, 0, &sound);		// FMOD_DEFAULT uses the defaults.  These are the same as FMOD_LOOP_OFF | FMOD_2D | FMOD_HARDWARE.
+	if (result != FMOD_OK)
+	{
+		printf("FMOD error! (%d) %s\n", result, FMOD_ErrorString(result));
+		return -1;
+	}
+
+	FMOD::Channel *channel;
+	result = fmodSystem->playSound(FMOD_CHANNEL_FREE, sound, false, &channel);
+	if (result != FMOD_OK)
+	{
+		printf("FMOD error! (%d) %s\n", result, FMOD_ErrorString(result));
+		return -1;
+	}
+
+	FMOD::DSP *dsp, *dsp2;
+	fmodSystem->createDSPByType(FMOD_DSP_TYPE_ECHO, &dsp);
+	fmodSystem->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT, &dsp2);
+	dsp2->setParameter(0, 100);
+	channel->addDSP(dsp, 0);
+	channel->addDSP(dsp2, 0);
+
+	Sleep(10000);
+
+	channel->stop();
+
+	return 0;
+}
+*/
+
--- trunk/Source/BismuthAudio/FmodAudioManager.h
+++ trunk/Source/BismuthAudio/FmodAudioManager.h
@@ -5,32 +5,32 @@
  * /____/_/___/_/_/_/\_,_/\__/_//_/\__/_//_/\_, /_/_//_/\__/
  *                                         /___/
  *
- * @file BismuthAudio.h
+ * @file FmodAudioManager.h
  */
 
 #pragma once
 
-#include <vector>
-#include "Entity.h"
-#include "AudioProperties.h"
+#include "AudioManager.h"
+#include "fmod.hpp"
 
 namespace Bismuth {
 	namespace Audio {
-
 		/**
-		 * BismuthAudio class
+		 * FmodAudioManager class
 		 */
-		class AudioManager {
+		class FmodAudioManager : public AudioManager {
 		public:
-			AudioManager();
-			virtual ~AudioManager();
-			
+			FmodAudioManager();
+			virtual ~FmodAudioManager();
+
 			/** 
-			* Plays a sound.
-			*
-			* @param entity The entity from which the sound comes from.
+			* Plays sound
 			*/
-			virtual void playSound(Entity &entity, AudioProperties &properties) = 0;
+			virtual void playSound(Entity &entity, AudioProperties &properties);
+
+
+		private:
+			FMOD::System *fmodSystem;
 		};
 	}
 }
