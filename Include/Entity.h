6b4a0a13f4e4d2e8bab4aebfb9daa38c22fb3b92
johan@tinyrocket.se <johan@tinyrocket.se@aefdbfa2-c794-11de-8410-e5b1e99fc78e>
2009-11-09 13:32:01 +0000

    Entity remake.

--- Include/Entity.h
+++ Include/Entity.h
@@ -15,9 +15,10 @@
 
 namespace Bismuth {
 
+	class IStream;
+
 	/**
 	 * The basic entity class, represent an object in the game world.
-	 * Every entity consists of a number of components.
 	 */
 	class Entity {
 	public:
@@ -25,81 +26,28 @@ namespace Bismuth {
 		virtual ~Entity();
 
 		/**
-		 * Adds a component
-		 * @param component Component to add
-		 * @throw std::logic_error If the component is already attached
+		 * Tick the entity once
+		 * @param elapsedTime Time in seconds since last update
 		 */
-		void addComponent(SharedPtr<IComponent> component);
+		void update(float elapsedTime);
 
-		/**
-		 * Removes a component
-		 * @param component Component to remove
-		 */
-		void removeComponent(SharedPtr<IComponent> component);
+		void setPosition(const Ogre::Vector3 &position) { this->position = position; }
+		Ogre::Vector3 getPosition() const { return position; }
 
-		/**
-		 * Checks if a specific component is attached
-		 * @param component Component to check for
-		 * @return True if the component is attached, false otherwise
-		 */
-		bool hasComponent(SharedPtr<IComponent> component) const;
+		void setOrientation(const Ogre::Quaternion &orientation) { this->orientation = orientation; }
+		Ogre::Quaternion getOrientation() const { return orientation; }
 
-		/**
-		 * Activates the entity when it's added to the entity manager
-		 */
-		void activate();
-
-		/**
-		 * Deactivates the entity when it's removed from the entity manager
-		 */
-		void deactivate();
-
-		/**
-		 * Check if the entity has a specific property
-		 * @param id Id of the property to check for
-		 * @return True if the property exists
-		 */
-		bool hasProperty(const PropertyID &id) const;
-
-		/**
-		 * Add a property, if the property already exists then nothing happens
-		 * @param id Id of the property to ad
-		 * @param defaultValue Default value to set if the property does not exist
-		 */
-		void addProperty(const PropertyID &id, Any defaultValue);
-
-		/**
-		 * Set a property, the property will be added if it does not exists
-		 * @param id Id of the property
-		 * @param value Value to set
-		 */
-		void setProperty(const PropertyID &id, Any value);
-
-		/**
-		 * Get the value of a property
-		 * @param id Id of the property to set
-		 * @return Value of the property
-		 * @throw std::logic_error If the property does not exist
-		 */
-		Any getProperty(const PropertyID &id);
-
-		/**
-		 * Set the unique identifier
-		 */
 		void setId(int id) { this->id = id; }
-
-		/**
-		 * Get the unique identifier
-		 */
 		int getId() const { return id; }
 
+		void serialize(SharedPtr<IStream> stream) const;
+		void deserialize(SharedPtr<IStream> stream);
+
 	private:
+		Ogre::Vector3 position;
+		Ogre::Quaternion orientation;
 		int id;
 
-		typedef std::vector<SharedPtr<IComponent> > IComponentList; 
-		IComponentList components;
-
-		PropertySet propertySet;
 	};
 
 }
