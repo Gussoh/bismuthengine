2561cce18680ddd86c8f741ae2763d3c5174e4ce
johan@tinyrocket.se <johan@tinyrocket.se@aefdbfa2-c794-11de-8410-e5b1e99fc78e>
2009-11-10 16:59:39 +0000

    Init Ogre!!!!1

---
+++ Source/Graphics/Renderer.cpp
@@ -0,0 +1,87 @@
+/**
+ * @file Renderer.cpp
+ */
+
+#include "stdafx.h"
+#include "Renderer.h"
+#include "OgreRoot.h"
+#include "OgreRenderSystem.h"
+#include "OgreRenderWindow.h"
+#include "OgreCamera.h"
+#include "OgreViewport.h"
+#include "OgreSceneManager.h"
+#include "OgreColourValue.h"
+#include "OgreWindowEventUtilities.h"
+
+using namespace Bismuth;
+using namespace Bismuth::Graphics;
+using namespace Ogre;
+
+Renderer::Renderer() : alive(false) {
+
+}
+
+Renderer::~Renderer() {
+
+}
+
+void Renderer::init(int width, int height, bool fullscreen) {
+	if (alive)
+	{
+		throw std::logic_error("renderer is already initialised");
+	}
+
+	Root *root = new Root("", "", "OgreLog.txt");
+
+	// Load the render system
+#ifdef _DEBUG
+	root->loadPlugin("RenderSystem_Direct3D9_d");
+#else
+	root->loadPlugin("RenderSystem_Direct3D9");
+#endif
+
+	// Select the d3d render system
+	root->setRenderSystem(root->getRenderSystemByName("Direct3D9 Rendering Subsystem"));
+	RenderSystem *rs = root->getRenderSystem();
+
+	rs->setConfigOption("Full Screen", StringConverter::toString(fullscreen));
+	rs->setConfigOption("Video Mode", StringConverter::toString(width) + " x " + StringConverter::toString(height) + " @ 32-bit colour");
+
+	root->initialise(true, "BismuthEngine");
+
+	RenderWindow *window = root->getAutoCreatedWindow();
+
+	alive = true;
+
+	// Select a default scene manager
+	SceneManager *sceneManager = root->createSceneManager(ST_EXTERIOR_CLOSE, "SceneManager");
+	sceneManager->setAmbientLight(ColourValue(0.6f, 0.6f, 0.6f));
+
+	// Create the default camera
+	camera = sceneManager->createCamera("defaultCamera");
+	camera->setNearClipDistance(0.1f);
+	camera->setFarClipDistance(512.0f);
+	camera->setFOVy(Degree(65.0f));
+	camera->setAspectRatio(width / (float)height);
+
+	// And a viewport
+	Viewport *vp = window->addViewport(camera);
+	vp->setBackgroundColour(ColourValue(0.2f, 0.4f, 0.3f, 1.0f));
+}
+
+void Renderer::kill() {
+	if (!alive)
+	{
+		throw std::logic_error("renderer is not initialised");
+	}
+
+	delete Root::getSingletonPtr();
+}
+
+bool Renderer::isWindowOpen() const {
+	return alive && !Root::getSingleton().getAutoCreatedWindow()->isClosed();
+}
+
+void Renderer::messagePump() {
+	WindowEventUtilities::messagePump();
+}
