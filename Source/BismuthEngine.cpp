cfb3868c774cd0db9b29234aac4e9c9f90eb378a
andreas.duchen <andreas.duchen@aefdbfa2-c794-11de-8410-e5b1e99fc78e>
2009-11-11 16:10:40 +0000

    Moved internal header files to Source\include

--- Source/BismuthEngine.cpp
+++ Source/BismuthEngine.cpp
@@ -4,12 +4,13 @@
 
 #include "stdafx.h"
 #include "BismuthEngine.h"
+#include "FmodAudioManager.h"
 
 using namespace Bismuth;
 using namespace Bismuth::Audio;
 
 BismuthEngine::BismuthEngine() {
-	this->audioManager = SharedPtr<AudioManager>();
+	this->audioManager = SharedPtr<AudioManager>(new FmodAudioManager());
 }
 
 BismuthEngine::~BismuthEngine() {
---
+++ Source/include/FmodAudioManager.h
@@ -0,0 +1,36 @@
+/**
+ *    ___  _                __  __                 _
+ *   / _ )(_)__ __ _  __ __/ /_/ /  ___ ___  ___ _(_)__  ___
+ *  / _  / (_-</  ' \/ // / __/ _ \/ -_) _ \/ _ `/ / _ \/ -_)
+ * /____/_/___/_/_/_/\_,_/\__/_//_/\__/_//_/\_, /_/_//_/\__/
+ *                                         /___/
+ *
+ * @file FmodAudioManager.h
+ */
+
+#pragma once
+
+#include "AudioManager.h"
+#include "fmod.hpp"
+
+namespace Bismuth {
+	namespace Audio {
+		/**
+		 * FmodAudioManager class
+		 */
+		class FmodAudioManager : public AudioManager {
+		public:
+			FmodAudioManager();
+			virtual ~FmodAudioManager();
+
+			/** 
+			* Plays sound
+			*/
+			virtual void playSound(const Entity &entity, const AudioProperties &properties);
+
+
+		private:
+			FMOD::System *fmodSystem;
+		};
+	}
+}
