46559383f68c1f96e6fc9e2151faad0827beaf5e
andreas.duchen <andreas.duchen@aefdbfa2-c794-11de-8410-e5b1e99fc78e>
2009-11-11 15:42:48 +0000

    Added physics engine interface

---
+++ trunk/Include/PhysicsManager.h
@@ -0,0 +1,36 @@
+/**
+ *    ___  _                __  __                 _
+ *   / _ )(_)__ __ _  __ __/ /_/ /  ___ ___  ___ _(_)__  ___
+ *  / _  / (_-</  ' \/ // / __/ _ \/ -_) _ \/ _ `/ / _ \/ -_)
+ * /____/_/___/_/_/_/\_,_/\__/_//_/\__/_//_/\_, /_/_//_/\__/
+ *                                         /___/
+ *
+ * @file PhysicsManager.h
+ */
+
+#pragma once
+
+#include "Entity.h"
+#include <vector>
+
+namespace Bismuth {
+	namespace Physics {
+		/**
+		 * PhysicsManager class
+		 */
+		class PhysicsManager {
+		public:
+			/**
+			* Finds all entities that within a given distance from an entity.
+			*
+			* @param radius The radius to use when finding nearby entities.
+			* @param source The source entity.
+			* @param[out] entityList Will be populated with entities that were found within the radius.
+			*/
+			virtual void getNearbyEntities(float radius, SharedPtr<Entity> source, std::vector<SharedPtr<Entity> > &entityList) = 0;
+			
+
+		private:
+		};
+	}
+}
