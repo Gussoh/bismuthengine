6ae97428f0d8fb1db290d37c6c6fca8d5918d492
johan@tinyrocket.se <johan@tinyrocket.se@aefdbfa2-c794-11de-8410-e5b1e99fc78e>
2009-11-23 11:55:03 +0000

    Simple edge detection in the shaders, does not work very well on objects without curves.

--- trunk/Assets/Shaders/Base.hlsl
+++ trunk/Assets/Shaders/Base.hlsl
@@ -1,10 +1,12 @@
 void WallVP(
 	float4 pos : POSITION,
+	float3 normal : NORMAL,
 	float2 uv : TEXCOORD0,
 	
 	out float4 oPos : POSITION,
 	out float2 oUv : TEXCOORD0,
 	out float oWallDistance : TEXCOORD1,
+	out float oEdge : TEXCOORD2,
 	
 	uniform float4x4 worldViewProj,
 	uniform float4x4 world,
@@ -17,18 +19,24 @@ void WallVP(
 	
 	// Get distance from the camera to the vertex
 	oWallDistance = length(cameraPos - worldPos);
+	
+	float3 N = normalize(normal);
+	float3 E = normalize(cameraPos - pos.xyz);
+	oEdge = max(dot(N, E), 0.0f);
 }
 
 void WallFP(
 	float2 uv : TEXCOORD0,
 	float wallDistance : TEXCOORD1,
+	float edge : TEXCOORD2,
 	
 	out float4 oColor : COLOR,
 	
 	//uniform float maxWallDistance,
 	//uniform float minAlpha,
 	
-	uniform sampler2D diffuse
+	uniform sampler2D diffuse,
+	uniform sampler1D edgeRamp
 ) {
 	const float maxWallDistance = 10.0f;
 	const float minAlpha = 0.0f;
@@ -38,6 +46,46 @@ void WallFP(
 	// Fragments closer to the camera are more transparent
 	float a = wallDistance / maxWallDistance;
 	a = saturate(minAlpha + a);
+	
+	edge = tex1D(edgeRamp, edge).x;
 
-	oColor = float4(c, a);
+	oColor = float4(edge * c, a);
+}
+
+void CellVP(
+	float4 pos : POSITION,
+	float3 normal : NORMAL,
+	float2 uv : TEXCOORD0,
+	
+	out float4 oPos : POSITION,
+	out float2 oUv : TEXCOORD0,
+	out float oEdge : TEXCOORD1,
+	
+	uniform float4x4 worldViewProj,
+	uniform float4 cameraPos
+) {
+	oPos = mul(worldViewProj, pos);
+	oUv = uv;
+	
+	float3 N = normalize(normal);
+	float3 E = normalize(cameraPos - pos.xyz);
+	oEdge = max(dot(N, E), 0.0f);
+}
+
+void CellFP(
+	float2 uv : TEXCOORD0,
+	float edge : TEXCOORD1,
+	
+	out float4 oColor : COLOR,
+	
+	//uniform float maxWallDistance,
+	//uniform float minAlpha,
+	
+	uniform sampler2D diffuse,
+	uniform sampler1D edgeRamp
+) {
+	float3 c = tex2D(diffuse, uv).rgb;
+	edge = tex1D(edgeRamp, edge).x;
+
+	oColor = float4(c, 1.0f);
 }
