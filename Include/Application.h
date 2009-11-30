a84ceaa6a2a53dab06b23fde0f1f8db3e05f5fdb
johan@tinyrocket.se <johan@tinyrocket.se@aefdbfa2-c794-11de-8410-e5b1e99fc78e>
2009-11-30 15:13:15 +0000

    Application class

---
+++ Include/Application.h
@@ -0,0 +1,57 @@
+/**
+ *    ___  _                __  __                 _
+ *   / _ )(_)__ __ _  __ __/ /_/ /  ___ ___  ___ _(_)__  ___
+ *  / _  / (_-</  ' \/ // / __/ _ \/ -_) _ \/ _ `/ / _ \/ -_)
+ * /____/_/___/_/_/_/\_,_/\__/_//_/\__/_//_/\_, /_/_//_/\__/
+ *                                         /___/
+ *
+ * @file Application.h
+ */
+
+#pragma once
+
+namespace Bismuth {
+	class GameLogic;
+
+	/**
+	 * Application class
+	 */
+	class Application {
+	public:
+		Application(bool isServer);
+		virtual ~Application();
+	
+		/**
+		 * Starts the application
+		 */
+		void run();
+
+		/**
+		 * Called before update
+		 * \param elapsedTime Time in seconds since last update
+		 */
+		virtual void update(float elapsedTime) {};
+
+		/**
+		 * Called before render
+		 */
+		virtual void prerender() {};
+
+		/**
+		 * Called after render
+		 */
+		virtual void postrender() {};
+
+		/**
+		 * Kill the application
+		 */
+		void kill();
+
+	private:
+		GameLogic *gameLogic;
+		bool running;
+		bool isServer;
+
+	};
+
+}
---
+++ Source/Application.cpp
@@ -0,0 +1,46 @@
+/**
+ * @file Application.cpp
+ */
+
+#include "stdafx.h"
+#include "Application.h"
+#include "GameLogic.h"
+#include "Renderer.h"
+#include "OgreTimer.h"
+
+using namespace Bismuth;
+
+Application::Application(bool isServer) : running(false), gameLogic(0), isServer(isServer) {
+	
+}
+
+Application::~Application() {
+
+}
+
+void Application::run() {
+	gameLogic = new GameLogic(isServer);
+	Graphics::Renderer *renderer = gameLogic->getRenderer();
+
+	Ogre::Timer timer;
+	while (running) {
+		float deltaTime = timer.getMilliseconds() * 0.001f;
+
+		this->update(deltaTime);
+		gameLogic->update(deltaTime);
+
+		this->prerender();
+		gameLogic->render();
+		this->postrender();
+
+		running = running && renderer->isWindowOpen();
+
+		timer.reset();
+	}
+
+	delete gameLogic;
+}
+
+void Application::kill() {
+	running = false;
+}
