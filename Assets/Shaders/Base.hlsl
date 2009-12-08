acf55c93fac022a8e8bdda85afe69b5bea00100e
johan@tinyrocket.se <johan@tinyrocket.se@aefdbfa2-c794-11de-8410-e5b1e99fc78e>
2009-12-08 14:01:24 +0000

    ninja!!!!!11111000

--- Assets/Shaders/Base.hlsl
+++ Assets/Shaders/Base.hlsl
@@ -7,41 +7,48 @@ void WallVP(
 	out float2 oUv : TEXCOORD0,
 	out float oWallDistance : TEXCOORD1,
 	out float oEdge : TEXCOORD2,
+	out float oDiffuse : TEXCOORD3,
 	
 	uniform float4x4 worldViewProj,
-	uniform float4x4 world,
-	uniform float4 cameraPos
+	uniform float4 cameraPos,
+	uniform float3 lightPosition
 ) {
 	oPos = mul(worldViewProj, pos);
 	oUv = uv;
 		
-	float4 worldPos = mul(world, pos);
-	
 	// Get distance from the camera to the vertex
 	oWallDistance = oPos.z;
 	
 	float3 N = normalize(normal);
 	float3 E = normalize(cameraPos - pos.xyz);
 	oEdge = max(dot(N, E), 0.0f);
+	
+	float3 L = normalize(lightPosition - pos.xyz);
+	oDiffuse = max(dot(N, L), 0.0f);
 }
 
 void WallFP(
 	float2 uv : TEXCOORD0,
 	float wallDistance : TEXCOORD1,
 	float edge : TEXCOORD2,
+	float diffuse : TEXCOORD3,
 	
 	out float4 oColor : COLOR,
 	
 	//uniform float maxWallDistance,
 	//uniform float minAlpha,
 	
-	uniform sampler2D diffuse,
-	uniform sampler1D edgeRamp
+	uniform sampler2D diffuseTexture,
+	uniform sampler1D edgeRamp,
+	uniform sampler1D diffuseRamp
 ) {
 	const float maxWallDistance = 2.5f;
 	const float minAlpha = 0.4f;
 
-	float3 c = tex2D(diffuse, uv).rgb;
+	float3 c = tex2D(diffuseTexture, uv).rgb;
+	
+	float d = tex1D(diffuseRamp, diffuse).x;
+	c = diffuse * d * c;
 
 	// Fragments closer to the camera are more transparent
 	float a = wallDistance / maxWallDistance;
@@ -61,9 +68,11 @@ void CellVP(
 	out float4 oPos : POSITION,
 	out float2 oUv : TEXCOORD0,
 	out float oEdge : TEXCOORD1,
+	out float oDiffuse : TEXCOORD2,
 	
 	uniform float4x4 worldViewProj,
-	uniform float4 cameraPos
+	uniform float4 cameraPos,
+	uniform float3 lightPosition
 ) {
 	oPos = mul(worldViewProj, pos);
 	oUv = uv;
@@ -71,21 +80,30 @@ void CellVP(
 	float3 N = normalize(normal);
 	float3 E = normalize(cameraPos - pos.xyz);
 	oEdge = max(dot(N, E), 0.0f);
+	
+	float3 L = normalize(lightPosition - pos.xyz);
+	oDiffuse = max(dot(N, L), 0.0f);
 }
 
 void CellFP(
 	float2 uv : TEXCOORD0,
 	float edge : TEXCOORD1,
+	float diffuse : TEXCOORD2,
 	
 	out float4 oColor : COLOR,
 	
 	//uniform float maxWallDistance,
 	//uniform float minAlpha,
 	
-	uniform sampler2D diffuse,
-	uniform sampler1D edgeRamp
+	uniform sampler2D diffuseTexture,
+	uniform sampler1D edgeRamp,
+	uniform sampler1D diffuseRamp
 ) {
-	float3 c = tex2D(diffuse, uv).rgb;
+	float3 c = tex2D(diffuseTexture, uv).rgb;
+	
+	float d = tex1D(diffuseRamp, diffuse).x;
+	c = diffuse * d * c;
+	
 	edge = tex1D(edgeRamp, edge).x;
 
 	oColor = float4(c, 1.0f);
